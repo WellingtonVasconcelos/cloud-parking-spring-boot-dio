aa5a835db4d7a0694b92dc6f7b2ba778398ca802
Wellington Vasconcelos <wltvasc@hotmail.com>
2022-08-19 16:12:55 -0300

    Definindo versão do Java

--- src/main/java/br/com/tom/cloud/CloudParkingApplication.java
+++ src/main/java/br/com/tom/cloud/CloudParkingApplication.java
@@ -2,8 +2,10 @@ package br.com.tom.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@RequestMapping("/")
 public class CloudParkingApplication {
 
 	public static void main(String[] args) {
