0bbaa8426b821aea98c8ba7da5ab98659fbaf5e3
Wellington Vasconcelos <wltvasc@hotmail.com>
2022-08-19 18:23:04 -0300

    Primeiro endpoint de lista

---
+++ src/main/java/br/com/tom/cloud/controller/mapper/ParkingMapper.java
@@ -0,0 +1,23 @@
+package br.com.tom.cloud.controller.mapper;
+
+import br.com.tom.cloud.controller.dto.ParkingDTO;
+import br.com.tom.cloud.model.Parking;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class ParkingMapper {
+
+    private static final ModelMapper MODEL_MAPPER = new ModelMapper();
+
+    public ParkingDTO parkingDTO(Parking parking){
+        return MODEL_MAPPER.map(parking, ParkingDTO.class);
+    }
+
+    public List<ParkingDTO> toParkingDTOList(List<Parking> parkingList) {
+        return parkingList.stream().map(this::parkingDTO).collect(Collectors.toList());
+    }
+}
